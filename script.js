645cb2cf40160bb1d7b26d4105fc5c38d138a936
shsobur <shsoburhossen951@gmail.com>
2025-03-05 12:59:37 +0600

    Theme seted

---
+++ script.js
@@ -0,0 +1,9 @@
+// Chenge theme__
+const themeColor = ["#fcadad", "#f2abf8", "#ffd799", "#c091ec"];
+let colorIndex = 0;
+
+const handleThemeColor = () => {
+  const body = document.querySelector("body");
+  body.style.backgroundColor = themeColor[colorIndex];
+  colorIndex = (colorIndex + 1) % themeColor.length;
+};
