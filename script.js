dc55648dc6521b0fd78eba3a0fdf3f0f5730d9ec
shsobur <shsoburhossen951@gmail.com>
2025-03-08 13:04:48 +0600

    Task counting function completed

--- script.js
+++ script.js
@@ -5,5 +5,31 @@ let colorIndex = 0;
 const handleThemeColor = () => {
   const body = document.querySelector("body");
   body.style.backgroundColor = themeColor[colorIndex];
-  colorIndex = (colorIndex + 1) % themeColor.length; 
+  colorIndex = (colorIndex + 1) % themeColor.length;
 };
+
+// Set current data__
+const date = {
+  weekday: "short",
+  month: "short",
+  day: "2-digit",
+  year: "numeric"
+}
+const currentDate = new Date().toLocaleDateString("en-BD", date);
+const dateElement = document.getElementById("currentDate");
+dateElement.innerText = currentDate;
+
+// Task__
+
+const handleTask = () => {
+  const taskCount = document.getElementById("task_number").innerText;
+  const convertedTadkCount = Number(taskCount);
+
+  if(convertedTadkCount < 1) {
+    return;
+  }
+
+  const remainingTaskCount = convertedTadkCount - 1;
+  const newTaskCount = document.getElementById("task_number");
+  newTaskCount.innerText = remainingTaskCount;
+}
