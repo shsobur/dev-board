10dba4d3bede2f59fbc43620196057ead2d66ce9
shsobur <shsoburhossen951@gmail.com>
2025-03-08 22:44:24 +0600

    Dashboard all functions completed

--- script.js
+++ script.js
@@ -13,8 +13,8 @@ const date = {
   weekday: "short",
   month: "short",
   day: "2-digit",
-  year: "numeric"
-}
+  year: "numeric",
+};
 const currentDate = new Date().toLocaleDateString("en-BD", date);
 const dateElement = document.getElementById("currentDate");
 dateElement.innerText = currentDate;
@@ -24,12 +24,14 @@ dateElement.innerText = currentDate;
 const handleTask = (clickedButton) => {
   const taskCount = document.getElementById("task_number").innerText;
   const convertedTaskCount = Number(taskCount);
-  const completedTaskCount = document.getElementById("complete_task_number").innerText;
+  const completedTaskCount = document.getElementById(
+    "complete_task_number"
+  ).innerText;
   const convertedCompletedTaskCount = Number(completedTaskCount);
 
   window.alert("Board updated successfully");
 
-  if(convertedTaskCount < 1) {
+  if (convertedTaskCount < 1) {
     return;
   }
 
@@ -37,14 +39,43 @@ const handleTask = (clickedButton) => {
   const newTaskCount = document.getElementById("task_number");
   newTaskCount.innerText = remainingTaskCount;
 
-  if(convertedCompletedTaskCount < 1) {
+  if (convertedCompletedTaskCount < 1) {
     return;
   }
 
-  const remainingCompletedTask = convertedCompletedTaskCount + 1
+  const remainingCompletedTask = convertedCompletedTaskCount + 1;
   const newCompletedTask = document.getElementById("complete_task_number");
   newCompletedTask.innerText = remainingCompletedTask;
 
+  if (convertedTaskCount === 1) {
+    window.alert("Congratulation!!! you have completed all the current task");
+  }
+
   clickedButton.disabled = true;
   clickedButton.classList.add("task_btn");
-}
+  handleComment(clickedButton);
+};
+
+// Comments__
+
+const handleComment = (clickedButton) => {
+  const commentParent = document.getElementById("comment_parent");
+  const currentTime = new Date().toLocaleTimeString("en-BD");
+
+  const taskContainer = clickedButton.closest(".task_box_main_container");
+  const taskTitle = taskContainer.querySelector(
+    ".task_box_inner_container h2"
+  ).innerText;
+
+  const newElement = document.createElement("p");
+  newElement.innerText = `You have Complete The Task ${taskTitle} at ${currentTime}`;
+  newElement.classList.add("comment");
+  commentParent.appendChild(newElement);
+};
+
+// Reset Comment__
+
+const handleResetComment = () => {
+  const commentContainer = document.getElementById("comment_parent");
+  commentContainer.remove();
+};
